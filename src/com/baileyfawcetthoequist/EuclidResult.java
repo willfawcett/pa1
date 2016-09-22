c3d37aca363a9b40ad1fe665723f5a6539ccd092
jessicabailey <jmb1323@jagmail.southalabama.edu>
2016-09-21 20:58:02 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/baileyfawcetthoequist/Main.java

--- src/com/baileyfawcetthoequist/EuclidResult.java
+++ src/com/baileyfawcetthoequist/EuclidResult.java
@@ -5,17 +5,17 @@ import java.util.Comparator;
 /**
  * Created by Bailey-Fawcett-Hoequist on 9/20/16.
  *
- * Container for a set of integers, their GCD, and the time in MS to calculate GCD using two different Euclid algorithms
- * Includes methods to computer the GCD using both algorithms
+ * Container for a set of integers, their GDC, and the time in MS to calculate GDC using two different Euclid algorithms
+ * Includes methods to computer the GDC using both algorithms
  *
  */
 public class EuclidResult {
     public long numberOne;   //randomly generated number, greater than 0 - using long to sidestep overflow problems
     public long numberTwo;   //randomly generated number, greater than 0 - using long to sidestep overflow problems
-    public long gcdV1;       //GCD as calculated by V1
-    public long gcdV2;       //GCD as calculated by V2 (should be the same in all cases, but space is cheap so why not...)
-    public long v1ns;       //time (nanoseconds) to calculate GCD using Euler's algorithm 1
-    public long v2ns;       //time (nanoseconds) to calculate GCD using Euler's algorithm 2 (improved)
+    public long gdcV1;       //GDC as calculated by V1
+    public long gdcV2;       //GDC as calculated by V2 (should be the same in all cases, but space is cheap so why not...)
+    public long v1ns;       //time (nanoseconds) to calculate GDC using Euler's algorithm 1
+    public long v2ns;       //time (nanoseconds) to calculate GDC using Euler's algorithm 2 (improved)
 
     public double v1ms;     //time in milliseconds for original algorithm
     public double v2ms;     //time for improved algorithm in milliseconds
@@ -28,31 +28,31 @@ public class EuclidResult {
         numberOne = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
         numberTwo = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
 
-        //calculate the GCD using Euclid's algorithm 1, and get the running time while we're at it
+        //calculate the GDC using Euclid's algorithm 1, and get the running time while we're at it
         startTime = System.nanoTime();
-        gcdV1 = euclidV1(numberOne, numberTwo);
+        gdcV1 = euclidV1(numberOne, numberTwo);
         v1ns = System.nanoTime() - startTime;
         v1ms = v1ns / 1000000.0;
         //System.out.println("v1: " + runningTime + "ns");
         //v1ms = TimeUnit.MICROSECONDS.convert(runningTime, TimeUnit.NANOSECONDS);
 
-        //calculate the GCD using Euclid's algorithm 2
+        //calculate the GDC using Euclid's algorithm 2
         startTime = System.nanoTime();
-        gcdV2 = euclidV2(numberOne, numberTwo);
+        gdcV2 = euclidV2(numberOne, numberTwo);
         v2ns = System.nanoTime() - startTime;
         v2ms = v2ns / 1000000.0;
         //System.out.println("v2: " + runningTime + "ns");
         //v2ms = TimeUnit.MICROSECONDS.convert(runningTime, TimeUnit.NANOSECONDS);
 
         //for debugging
-        //System.out.println(numberOne + " " + numberTwo + " " + gcdV1 + " " + gcdV2 + " " + "v1ms:" +v1ms + "v2ms:" + v2ms);
+        //System.out.println(numberOne + " " + numberTwo + " " + gdcV1 + " " + gdcV2 + " " + "v1ms:" +v1ms + "v2ms:" + v2ms);
     }
 
     /**
      * Euclid's Algorithm v1
-     * returns the GCD of two input integers
+     * returns the GDC of two input integers
      * returns 0 on invalid input
-      */
+     */
     static long euclidV1(long a, long b){
         long quotient;
         long remainder;
@@ -68,12 +68,12 @@ public class EuclidResult {
             a = b;
             b = remainder;
         }
-        return a; //this is the GCD
+        return a; //this is the GDC
     }
 
     /**
      * Euclid's Algorithm v2 (improved)
-     * returns the GCD of two input integers
+     * returns the GDC of two input integers
      * returns 0 on invalid input
      */
     static long euclidV2(long a, long b){
@@ -105,7 +105,7 @@ public class EuclidResult {
             a = b;
             b = remainder;
         }
-        return a;  //this is the GCD
+        return a;  //this is the GDC
     }
 
 
--- src/com/baileyfawcetthoequist/Main.java
+++ src/com/baileyfawcetthoequist/Main.java
@@ -17,34 +17,44 @@ public class Main {
         List<Long> v1Runtimes = new ArrayList<>();
         List<Long> v2Runtimes = new ArrayList<>();
 
-	// write your code here
-        System.out.println("~EUCLID'S COMPARATOR~\n");
+        double sum; //for calculating averages
+        double v1Avg;
+        double v2Avg;
+        Long v1Min;
+        Long v2Min;
+        Long v1Max;
+        Long v2Max;
+        Long v1Median;
+        Long v2Median;
+
+        // write your code here
+        System.out.println("~EUCLIDS COMPARATOR~\n");
 
         int pairsCount = 100;
         EuclidResult[] euclidResults = new EuclidResult[pairsCount];
 
         //GENERATE 100 pairs of random integers
         for(int i = 0; i < pairsCount; i++){
-            euclidResults[i] = new EuclidResult(); //GCD is computed and timed in constructor
+            euclidResults[i] = new EuclidResult(); //GDC is computed and timed in constructor
             num1List.add(euclidResults[i].numberOne);
             num2List.add(euclidResults[i].numberTwo);
-            gcdV1List.add(euclidResults[i].gcdV1);
-            gcdV2List.add(euclidResults[i].gcdV2);
+            gcdV1List.add(euclidResults[i].gdcV1);
+            gcdV2List.add(euclidResults[i].gdcV2);
             v1Runtimes.add(euclidResults[i].v1ns);
             v2Runtimes.add(euclidResults[i].v2ns);
         }
 
-        System.out.format("\n/ORIGINAL STATS/\n\n%-15s\t%-15s\n","Statistics","Microseconds");
+        System.out.format("\n/ORIGINAL STATS/\n%-15s\t%-15s\n","Statistics","Microseconds");
         printTable(v1Runtimes);
-        System.out.format("\n/MODIFIED STATS/\n\n%-15s\t%-15s\n","Statistics","Microseconds");
+        System.out.format("\n/MODIFIED STATS/\n%-15s\t%-15s\n","Statistics","Microseconds");
         printTable(v2Runtimes);
 
 
-        System.out.format("\n/ORIGINAL/\n\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ms)");
+        System.out.format("\n/ORIGINAL/\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ns)");
         for(int i = 0; i < pairsCount; i++){
             System.out.format("%-10d\t%-10d\t%-10d\t%-10f\n",num1List.get(i),num2List.get(i), gcdV1List.get(i), ((float)v1Runtimes.get(i)));
         }
-        System.out.format("\n/MODIFIED/\n\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ms)");
+        System.out.format("\n/MODIFIED/\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ns)");
         for(int i = 0; i < pairsCount; i++){
             System.out.format("%-10d\t%-10d\t%-10d\t%-10f\n",num1List.get(i),num2List.get(i), gcdV2List.get(i), ((float)v2Runtimes.get(i)));
         }
@@ -65,19 +75,87 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+        /***
+         *  VERSION 1 STATS
+         */
         //SORT the array based on V1 algorithm running time
         Arrays.sort(euclidResults, new Comparator<EuclidResult>() {
             public int compare(EuclidResult er1, EuclidResult er2) {
                 return toIntExact(er1.v1ns - er2.v1ns);
             }
         });
+        //Output list and Generate stats for V1
+        sum = 0.0;
+        System.out.format("\n/ORIGINAL/\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ms)");
+        for(int i = 0; i < pairsCount; i++){
+            EuclidResult e = euclidResults[i];
+            sum += e.v1ms;
+            System.out.format("%-10d\t%-10d\t%-10d\t%-10f\n",e.numberOne,e.numberTwo, e.gdcV1, e.v1ms);
+        }
+
+        //get stats for v1 (original algorithm)
+        v1Min = euclidResults[0].v1ns;
+        v1Max = euclidResults[pairsCount-1].v1ns;
+        v1Avg = sum / pairsCount;
+        v1Median = (euclidResults[49].v1ns + euclidResults[50].v1ns) / 2;
+
+        //display stats
+        System.out.format("\n/ORIGINAL STATS/ (ms)\n%-10s\t%-10s\t%-10s\t%-10s\n","Min","Max", "Average", "Median");
+        System.out.format("%-10f\t%-10f\t%-10f\t%-10f\n",v1Min / 1000000.0,v1Max / 1000000.0, v1Avg, v1Median / 1000000.0);
+
+        /***
+         * VERSION 2 STATS - still has a bug
+         */
+        //RE-SORT the array based on V2 algorithm running time
+        Arrays.sort(euclidResults, new Comparator<EuclidResult>() {
+            public int compare(EuclidResult er1, EuclidResult er2) {
+                return toIntExact(er1.v2ns - er2.v2ns);
+            }
+        });
+        //Output list and Generate stats for V2
+        sum = 0.0;
+        System.out.format("IMPROVED\n%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two", "GCD", "Runtime (ms)");
+        for(int i = 0; i < pairsCount; i++){
+            EuclidResult e = euclidResults[i];
+            sum += e.v2ms;
+            System.out.format("%-10d\t%-10d\t%-10d\t%-10f\n",e.numberOne,e.numberTwo, e.gdcV2, e.v2ms);
+        }
+
+        //get stats for v1 (original algorithm)
+        v2Min = euclidResults[0].v2ns;
+        v2Max = euclidResults[pairsCount-1].v2ns;
+        v2Avg = sum / pairsCount;
+        v2Median = (euclidResults[49].v2ns + euclidResults[50].v2ns) / 2;
+
+        //display stats
+        System.out.format("ORIGINAL STATS (ms)\n%-10s\t%-10s\t%-10s\t%-10s\n","Min","Max", "Average", "Median");
+        System.out.format("%-10f\t%-10f\t%-10f\t%-10f\n",v2Min / 1000000.0,v2Max / 1000000.0, v2Avg, v2Median / 1000000.0);
+
+        //UPDATED FOR VERSION 2
+        //Conclusions section.  So far tells how many times the modified version takes less time than the original version
+        //Compares the runtimes using .get() and if the original(a) is larger than the modified(b) then the modified(b) outperformed the original(a)
+        //Prints the conclusion to the screen
+        System.out.format("\n/CONCLUSIONS/\n");
+        int updatev2Count = 0;
+        for(int i = 0; i < pairsCount; i++) {
+            EuclidResult e = euclidResults[i];
+            if(e.v1ms > e.v2ms) {
+                updatev2Count += 1;
+            }
+        }
+        System.out.format("Out of %d pairs of integers, the modified Euclid's algorithm outperformed the original in %d pairs.", pairsCount, updatev2Count);
 
 
-        //Generate stats for V1
 
-        //SORT the array based on V2 algorithm running time
 
-        //Generate stats for V2
+
 
         //Generate stats for comparison
 
@@ -102,7 +180,7 @@ public class Main {
             median = runtimes.get(index)*1.0;
         }
         else{
-            median = (runtimes.get(index) + runtimes.get(index-1))/2;
+            median = (runtimes.get(index) + runtimes.get(index-1))/2.0;
         }
         System.out.format("%-15s\t%-15f\n","Maximum Time",runtimes.get(runtimes.size()-1)/1.0);
         System.out.format("%-15s\t%-15f\n","Minimum Time",runtimes.get(0)/1.0);
@@ -111,5 +189,4 @@ public class Main {
     }
 
 }
-
 
